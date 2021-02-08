b3ac74c2e587a1dfc351bf9e9e5b2cad2c61e787
n30config <mu-silva@outlook.com>
2021-02-08 17:45:31 -0300

    Utilizando Angular Material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContatoComponent } from './contato/contato.component';
 import {ContatoService} from './contato.service'
+import {MatButtonModule} from '@angular/material/button';
+
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +17,9 @@ import {ContatoService} from './contato.service'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [

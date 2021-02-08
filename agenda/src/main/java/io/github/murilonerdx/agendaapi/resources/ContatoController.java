7bf583807424eb9b03f95d3efc3d5c41443d928e
n30config <mu-silva@outlook.com>
2021-02-08 18:07:15 -0300

    Modificações para cors

--- agenda/src/main/java/io/github/murilonerdx/agendaapi/resources/ContatoController.java
+++ agenda/src/main/java/io/github/murilonerdx/agendaapi/resources/ContatoController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value ="/api/contatos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ContatoController {
     private final ContatoRepository repository;
 
